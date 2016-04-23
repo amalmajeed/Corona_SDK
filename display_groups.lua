09228ee645f1fd9710f7d1d559fd57feb412cca2
Amal Majeed M A <amalmajeed7@gmail.com>
2016-04-23 15:46:55 +0530

    Added Snowfall Transition

--- display_groups.lua
+++ display_groups.lua
@@ -18,5 +18,17 @@ group1:insert(luaLogo)
 --TEXT--
 text=display.newText( group1,"AMAL MAJEED",200,900)  -- YOU CAN ALSO INSERT ELEMENTS TO A GROUP BY SPECIFYING THE PARENT GROUP NAME IN THE ELEMENT PARAMETER
 text:scale(2,2)
-
 group1.x=500
+--group1:rotate(-20)	
+
+--SNOW FALL TRANSITIONS --
+snow=display.newGroup( )
+for i=1,26,1 do
+	circle=display.newCircle( math.random( 0,900 ), math.random( 0,900 ),15 )
+	snow:insert(circle)
+end
+
+snow.x=400
+snow.y=100
+
+transition.to( snow,{y=snow.y+500,time=5000})
