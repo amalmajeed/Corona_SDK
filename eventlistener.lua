b9a28e97ec1fd66e011a3f225c399617242c807e
Amal Majeed M A <amalmajeed7@gmail.com>
2016-04-27 10:26:39 +0530

    added event listener

---
+++ eventlistener.lua
@@ -0,0 +1,22 @@
+group1=display.newGroup( )
+Image1 = display.newImage("SampleImage.png",500,500)
+Image2 = display.newImage("SampleImage.png",700,500)
+Image2:scale( 0.5,0.5 )
+--WHAT HAPPENS WHEN IMAGE IS TOUCHED--
+
+Rectangle=display.newRect( group1,200,200,200,200 )
+--WE ARE MAKING THE IMAGE DRAGGABLE--
+function imageTouch(event)
+	if event.phase=="began" then      --A New Touch Event Has Begun
+		display.getCurrentStage( ):setFocus(event.target)
+	end
+	if event.phase=="moved" then      --A New Touch Event Has Begun
+		event.target.x=event.x
+		event.target.y=event.y
+	end
+	if event.phase=="ended" then
+		display.getCurrentStage( ):setFocus(nil)
+	end
+end
+Image1:addEventListener( "touch",imageTouch )
+Image2:addEventListener( "touch",imageTouch )
